1968531564e6bd4d4ac06f0b1a8b1811a2fc31d1
emmanueltotimeh2002 <emmanueltotimeh2002@gmail.com>
2024-12-03 22:11:54 +0000

    Bug fixes

--- p1/demo/src/main/java/com/example/demo/controller/UserController.java
+++ p1/demo/src/main/java/com/example/demo/controller/UserController.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,28 +23,31 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    
+    @GetMapping    
     public List <User> getAllUsers () {
         return userService.getAllUsers();
     }
     
-    
+    @GetMapping("/{id}")
     public ResponseEntity <User> getUserById(@PathVariable Long id) {
         return userService.getUserById(id).map(
             ResponseEntity::ok
         ).orElse(ResponseEntity.notFound().build());
     }
 
+    @PostMapping
     public User createUser (@RequestBody User user) {
         return userService.createUser(user);
     }
 
+    @PutMapping("/{id}")
     public ResponseEntity <User> updateUser (@PathVariable Long id, @RequestBody User user) {
         return userService.updateUser(id, user).map(
             ResponseEntity::ok
         ).orElse(ResponseEntity.notFound().build());
     }
 
+    @DeleteMapping("/{id}")
     public void deleteUser (@PathVariable Long id) {
         userService.deleteUser(id);
     }
